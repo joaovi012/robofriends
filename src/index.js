2d6ae168b1166b3efa3227b7d47a74865d9894d7
JV <joabeninca@hotmail.com>
2021-07-26 18:28:22 -0300

    dynamic robots

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import Card from './Card.js'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import {robots } from './robots.js'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Card />
+    <Card id = {robots[0].id} name = {robots[0].name} email = {robots[0].email} />
+    <Card id = {robots[1].id} name = {robots[1].name} email = {robots[1].email} />
+    <Card id = {robots[2].id} name = {robots[2].name} email = {robots[2].email} />
   </React.StrictMode>,
   document.getElementById('root')
 );
