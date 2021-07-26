486f1a6090970939753218bf420b31d3af323f2b
JV <joabeninca@hotmail.com>
2021-07-26 17:25:43 -0300

    Changes to App Class

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  render() {
+    return (<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,8 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
+    </div>)
+  };
 }
 
 export default App;
